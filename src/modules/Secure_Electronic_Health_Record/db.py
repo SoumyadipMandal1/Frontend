5a61689bd85959cdf3075abe2271493faf5fdde6
vprathikiit <24je0716@iitism.ac.in>
2026-03-25 21:32:13 +0530

    Prevent mongodb connection string from leaking

--- src/modules/Secure_Electronic_Health_Record/db.py
+++ src/modules/Secure_Electronic_Health_Record/db.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 @st.cache_resource
 def get_db():
-    connection_string = "mongodb+srv://vemsaiprathikreddy_db_user:PrAtHiK123@cluster0.ktpsj2n.mongodb.net/clinical_db?appName=Cluster0"
+    connection_string = 
     client = pymongo.MongoClient(connection_string)
     db = client["clinical_db"]
     return db
